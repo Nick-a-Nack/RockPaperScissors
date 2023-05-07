d83372f97e389d9893fdc421ce6950e9ca8b180b
Nicholas Cox <118236483+Nick-a-Nack@users.noreply.github.com>
2023-05-07 02:58:52 -0700

    - Added skeleton HTML for testing JS logic in console - Added getComputerMove function. It generates a random move between Rock, Paper, or Scissors and returns it as a string.

---
+++ javascript.js
@@ -0,0 +1,17 @@
+function getComputerChoice(){
+    
+    let choice = (Math.floor(Math.random()*100) % 3) + 1;
+    console.log(choice);
+    if (choice === 1){
+        return "rock";
+    }
+    else if (choice === 2){
+        return "paper";
+    }
+    else if (choice === 3){
+        return "scissors";
+    }
+    else{
+        return null;
+    }
+}
