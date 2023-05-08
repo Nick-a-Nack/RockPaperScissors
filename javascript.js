3b2bd017dc9a75a6d6554a7796f04b3f85d20c94
Nicholas Cox <118236483+Nick-a-Nack@users.noreply.github.com>
2023-05-07 19:30:14 -0700

    - Added functions playRound and game -- playRound includes win/loss logic for Rock  Paper Scissors game and -- some input validation to ensure a valid player move - gameFunction is not finished yet, but will loop over 5 rounds of playRound while tracking player and computer wins

--- javascript.js
+++ javascript.js
@@ -14,3 +14,32 @@ function getComputerChoice(){
         return null;
     }
 }
+
+function playRound(playerSelection, computerSelection){
+    //playerSelection is set to lowercase
+    let playerMove = playerSelection.toLowerCase();
+    if (playerMove != "rock" || playerMove != "paper" || playerMove != "scissors"){
+        return `Invalid player move "${playerMove}"`;
+    }
+    if (playerMove == computerSelection){
+        return `It's a draw! You and the computer both picked ${playerSelection}`;
+    }
+    else if ((playerMove == "paper" && computerSelection == "rock") ||
+             (playerMove == "rock" && computerSelection == "scissors") ||
+             (playerMove == "scissors" && computerSelection == "paper")){
+                return `You won! ${playerMove} beats ${computerSelection}`;
+             }
+    else{
+        return `You lose! ${computerSelection} beats ${playerMove}`;
+    }
+}
+
+function game(){
+    let playerWin = 0;
+    let computerWin = 0;
+    let roundCount = 0;
+    
+    while (++roundCount < 6){
+        
+    }
+}
